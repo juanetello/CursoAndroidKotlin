24f0545ffd02166dbd3dacd3a4f47319b989bde2
Juan Tello <juanetello.54@gmail.com>
2020-05-07 22:30:54 -0300

    Calculadora kotlin

--- Calculadora/app/src/main/java/ar/com/app/calculadora/MainActivity.kt
+++ Calculadora/app/src/main/java/ar/com/app/calculadora/MainActivity.kt
@@ -2,11 +2,37 @@ package ar.com.app.calculadora
 
 import android.support.v7.app.AppCompatActivity
 import android.os.Bundle
+import android.view.View
+import kotlinx.android.synthetic.main.activity_main.*
 
 class MainActivity : AppCompatActivity() {
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+    }
+
+    fun sum (view : View){
+        val  value1 = editText.text.toString().toDouble()
+        val  value2 = editText2.text.toString().toDouble()
+        textViewResultado.text = "Resultado: ${value1 + value2}"
+    }
+
+    fun sub (view : View){
+        val  value1 = editText.text.toString().toDouble()
+        val  value2 = editText2.text.toString().toDouble()
+        textViewResultado.text = "Resultado: ${value1 - value2}"
+    }
+
+    fun multi (view: View){
+        val  value1 = editText.text.toString().toDouble()
+        val  value2 = editText2.text.toString().toDouble()
+        textViewResultado.text = "Resultado: ${value1 * value2}"
+    }
+
+    fun div (view: View){
+        val  value1 = editText.text.toString().toDouble()
+        val  value2 = editText2.text.toString().toDouble()
+        textViewResultado.text = "Resultado: ${value1 / value2}"
     }
 }
